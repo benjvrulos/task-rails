d3980a5ea6623c2f4ec1e0dc443bd89ed9ff0026
benjv_tdh <cortesbenja@gmail.com>
2024-06-14 13:23:32 -0400

    validations for projects and tasks

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,11 @@
 class Project < ApplicationRecord
   belongs_to :user
   has_many :tasks,dependent: :destroy
+  validate :title_must_be_valid
+
+
+  private
+    def title_must_be_valid
+      errors.add(:base, "El nombre del proyecto es obligatorio") and return unless title.present?
+    end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,9 @@
 class Task < ApplicationRecord
   belongs_to :project
+  validate :title_must_be_valid
+
+  private
+    def title_must_be_valid
+      errors.add(:base, "El nombre de la tarea es obligatorio") and return unless title.present?
+    end
 end
